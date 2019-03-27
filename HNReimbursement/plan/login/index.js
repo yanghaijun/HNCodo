2834927559fabb647bf03cae41655d78ce7d5259
735539908 <735539908@qq.com>
2019-03-27 14:54:21 +0800

    fixed

---
+++ HNReimbursement/plan/login/index.js
@@ -0,0 +1,10 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control0_MNFEsK: function (elem) {},
+    doAction_uiControl0_HCHJWN: function (data, elem) {},
+    getTemplate_uiControl0_HCHJWN: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    return (\n      <div className='login'>\n        <div>\u534E\u80FD\u96C6\u56E2\u7F51\u4E0A\u62A5\u9500\u7CFB\u7EDF</div>\n        <div>\n        \t<AInput value='' />\n          <AInput value='' />\n        </div>\n        <div>\n        \t<button>\u91CD\u7F6E</button>\n          <button>\u767B\u5F55</button>\n        </div>\n      </div>\n    )\n  }\n});";
+      return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n\n  render: function render() {\n    return React.createElement(\n      'div',\n      { className: 'login' },\n      React.createElement(\n        'div',\n        null,\n        '\\u534E\\u80FD\\u96C6\\u56E2\\u7F51\\u4E0A\\u62A5\\u9500\\u7CFB\\u7EDF'\n      ),\n      React.createElement(\n        'div',\n        null,\n        React.createElement(AInput, { value: '' }),\n        React.createElement(AInput, { value: '' })\n      ),\n      React.createElement(\n        'div',\n        null,\n        React.createElement(\n          'button',\n          null,\n          '\\u91CD\\u7F6E'\n        ),\n        React.createElement(\n          'button',\n          null,\n          '\\u767B\\u5F55'\n        )\n      )\n    );\n  }\n});";
+    }
+  }, "login");
+})(window, ysp);
