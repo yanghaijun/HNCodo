b47e572e059be68b445612cff466e380b6eae464
735539908 <735539908@qq.com>
2019-04-10 09:57:15 +0800

    修改代办页问题和添加匹配规则

--- HN-OA/plan/index/index.js
+++ HN-OA/plan/index/index.js
@@ -11,7 +11,11 @@
     },
     doAction_uiControl0_lFVvUR: function (data, elem) {
       if (data.eventType == "click") {
-        var index = parseInt(data.customData) + 1;elem.querySelectorAll("tr")[index].querySelectorAll("td")[1].querySelector("a").click();
+        var index = parseInt(data.customData);var a = [];var trs = elem.querySelectorAll("tr");for (var i = 1; i < trs.length; i++) {
+          var title = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[1].querySelector("a") && trs[i].querySelectorAll("td")[1].querySelector("a").textContent;if (title.indexOf("发文") > -1 || title.indexOf("收文") > -1 || title.indexOf("签报") > -1) {
+            a.push(trs[i]);
+          }
+        }a[index].querySelectorAll("td")[1].querySelector("a").click(); //elem.querySelectorAll("tr")[index].querySelectorAll("td")[1].querySelector("a").click();
       }
     },
     getTemplate_uiControl0_lFVvUR: function () {
