1632a61f108001c7cc88bfb61c787ca1077139d5
darry <15135720474@163.com>
2019-07-31 17:09:58 +0800

    选人节点修改，对勾获取

--- HNYuHuan-OA/plan/content/index.js
+++ HNYuHuan-OA/plan/content/index.js
@@ -94,11 +94,11 @@
             //请选择用户
             if ($(t).find('legend').next()[0] && $(t).find('legend').next()[0].tagName == 'P') {
               $(t).find('p').map(function (q, l) {
-                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l)[0].getAttribute("selected") == 'noSelected' || $(l)[0].getAttribute("selected") == null ? 'false' : 'true';obj.type = 'P';data.selectApar.push(obj);
+                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l).text().indexOf('√') > -1 ? 'true' : 'false';obj.type = 'P';data.selectApar.push(obj);
               });
             } else if ($(t).find('legend').next()[0] && $(t).find('legend').next()[0].tagName == 'SELECT') {
               $(t).find('option').map(function (q, l) {
-                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l)[0].getAttribute("selected") == 'noSelected' || $(l)[0].getAttribute("selected") == null ? 'false' : 'true';obj.trueFalseFalg = $(l).attr("class") == null ? "false" : "true";obj.type = 'OPTION';data.selectApar.push(obj);
+                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l).text().indexOf('√') > -1 ? 'true' : 'false';obj.trueFalseFalg = $(l).attr("class") == null ? "false" : "true";obj.type = 'OPTION';data.selectApar.push(obj);
               });
             } else if ($(t).find('legend').next()[0] && $(t).find('legend').next()[0].tagName == 'DIV') {
               $(t).find('p').map(function (q, l) {
@@ -108,7 +108,8 @@
                   });data.textArr.bm.push(b);
                 }var rygroup = $(l).find('span[class="userlist groups"]') && $(l).find('span[class="userlist groups"]')[0] && $(l).find('span[class="userlist groups"]')[0].style && $(l).find('span[class="userlist groups"]')[0].style.display == "" && $(l).find('span[class="userlist groups"]').children();var ryuser = $(l).find('span[class="userlist users"]') && $(l).find('span[class="userlist users"]')[0] && $(l).find('span[class="userlist users"]')[0].style && $(l).find('span[class="userlist users"]')[0].style.display == "" && $(l).find('span[class="userlist users"]').children();if (rygroup && rygroup.length > 0) {
                   var user;rygroup.map(function (i, d) {
-                    var dValue = $(d).find("span");dValue.map(function (index, data) {
+                    var dValue = $(d).find("span");
+                    dValue.map(function (index, data) {
                       user = data.textContent + "、";a.push(user);
                     });
                   });data.textArr.ry.push(a);
@@ -116,9 +117,7 @@
                   var user;ryuser.map(function (i, d) {
                     user = d.textContent + "、";a.push(user);
                   });data.textArr.ry.push(a);
-                } // obj.text = $(l).text().replace(/\√/g, "");
-                // obj.trueFalse = $(l)[0].getAttribute("selected") == 'noSelected' || $(l)[0].getAttribute("selected") == null ? 'false' : 'true';
-                // obj.type = 'P';
+                }
               });
             }
           } else if (d == 1) {
@@ -133,7 +132,7 @@
             } //树结构结束
             if (tag == 'SELECT') {
               $(t).find('option').map(function (q, l) {
-                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l)[0].getAttribute("selected") == 'noSelected' || $(l)[0].getAttribute("selected") == null ? 'false' : 'true';obj.trueFalseFalg = $(l).attr("class") == null ? "false" : "true";obj.type = 'OPTION';data.selectAparPM.push(obj);
+                var obj = {};obj.text = $(l).text().replace(/\√/g, "");obj.trueFalse = $(l).text().indexOf('√') > -1 ? 'true' : 'false';obj.trueFalseFalg = $(l).attr("class") == null ? "false" : "true";obj.type = 'OPTION';data.selectAparPM.push(obj);
               });
             } //选择部门（选择后打√），默认部门结束
           }
@@ -216,22 +215,16 @@
         var data = data.customData;var iframes = $(elem).find('.ui_border').find('.ui_content').find('.ui_loading').next()[0].contentDocument.documentElement;var selectflowuser = $(iframes).find('#selectflowuser').find('fieldset').eq(2);if (selectflowuser.children()[1].tagName == 'P') {
           var selects = selectflowuser.find('select') && selectflowuser.find('p');
         } else {
-          var selects = selectflowuser.find('select') && selectflowuser.find('select').find('option')[data];$(selects).mousedown().click().mouseup();selects.selected = true;if (selects.getAttribute('selected') != 'selected') {
-            selects.setAttribute("selected", "selected");
-          } else {
-            selects.setAttribute("selected", "noSelected");
-          }
+          var selects = selectflowuser.find('select') && selectflowuser.find('select').find('option')[data]; // $(selects).mousedown().click().mouseup();
+          selects.selected = true;selects.setAttribute('class', 'selected');var evt = document.createEvent('MouseEvent');evt.initMouseEvent('change', true, true);selects.dispatchEvent(evt);
         } //选人的多选
       } else if (data.eventType == 'radioClickBm') {
-        var data = data.customData;var iframes = $(elem).find('.ui_border').find('.ui_content').find('.ui_loading').next()[0].contentDocument.documentElement;
-        var selectflowuser = $(iframes).find('#selectflowuser').find('fieldset').eq(1);if (selectflowuser.children()[1].tagName == 'P') {
+        var data = data.customData;var iframes = $(elem).find('.ui_border').find('.ui_content').find('.ui_loading').next()[0].contentDocument.documentElement;var selectflowuser = $(iframes).find('#selectflowuser').find('fieldset').eq(1);if (selectflowuser.children()[1].tagName == 'P') {
           var selects = selectflowuser.find('select') && selectflowuser.find('p');
         } else {
-          var selects = selectflowuser.find('select') && selectflowuser.find('select').find('option')[data];$(selects).mousedown().click().mouseup(); // if (selects.getAttribute('selected') != 'selected') {
-          //   selects.setAttribute("selected", "selected");
-          // } else {
-          //   selects.setAttribute("selected", "noSelected");
-          // }
+          var selects = selectflowuser.find('select') && selectflowuser.find('select').find('option')[data]; //$(selects).mousedown().click().mouseup(); 
+          selects.selected = true;
+          selects.setAttribute('class', 'selected');var evt = document.createEvent('MouseEvent');evt.initMouseEvent('change', true, true);selects.dispatchEvent(evt);
         } //选人的多选
       } else if (data.eventType == 'flowTitle') {
         var data = data.customData;var iframes = $(elem).find('.ui_border').find('.ui_content').find('.ui_loading').next()[0] && $(elem).find('.ui_border').find('.ui_content').find('.ui_loading').next()[0].contentDocument.documentElement;var trees = $(iframes).find('#selectflowuser') && $(iframes).find('#selectflowuser').find('.t_root');var domtree = trees.find('.t_node');domtree.eq(data).find('label').eq(0).find('input').click();
@@ -240,7 +233,8 @@
       }var type = data.eventType;if (type == "textareaChange") {
         var text = data.customData;elem.ownerDocument.querySelector('textarea[name="fldYijian"]').value = text;
       }if (type == "yijian") {
-        var index = parseInt(data.customData);var list = elem.ownerDocument.querySelector("#common_view_list");var lis = list && list.querySelectorAll("li");lis && lis[index].querySelector("a").click();
+        var index = parseInt(data.customData);
+        var list = elem.ownerDocument.querySelector("#common_view_list");var lis = list && list.querySelectorAll("li");lis && lis[index].querySelector("a").click();
       }if (type == "ZC") {
         var a = [];var release = elem.ownerDocument.querySelector(".Release");var lis = release && release.querySelectorAll("li");for (var i = 0; i < lis.length; i++) {
           if ("暂存" == lis[i].querySelector("span").textContent) {
@@ -255,15 +249,14 @@
           }
         }a && a[0].querySelector("a").click();
       }if (type == "THClose") {
-        var ss = [];
-        var ass = elem.ownerDocument.querySelector(".ui_title_buttons") && elem.ownerDocument.querySelector(".ui_title_buttons").querySelectorAll("a");for (var i = 0; i < ass.length; i++) {
+        var ss = [];var ass = elem.ownerDocument.querySelector(".ui_title_buttons") && elem.ownerDocument.querySelector(".ui_title_buttons").querySelectorAll("a");for (var i = 0; i < ass.length; i++) {
           if ("关闭(esc键)" == ass[i].getAttribute("title")) {
             ss.push(ass[i]);
           }
-        }ss && ss[0].click();
+        }
+        ss && ss[0].click();
       }if (type == "THTitle") {
-        var index = data.dataCustom;
-        var loading = elem.ownerDocument.querySelector(".ui_loading");var iframe = loading && loading.nextElementSibling;var datagrid = iframe && iframe.contentDocument.querySelector(".datagrid-view2");if (datagrid != null) {
+        var index = data.dataCustom;var loading = elem.ownerDocument.querySelector(".ui_loading");var iframe = loading && loading.nextElementSibling;var datagrid = iframe && iframe.contentDocument.querySelector(".datagrid-view2");if (datagrid != null) {
           var trs = datagrid.querySelector('table[class="datagrid-btable"]').querySelector("tbody").querySelectorAll("tr");trs && trs[index].click();
         }var dialog = elem.ownerDocument.querySelector('table[class="ui_dialog"]') && elem.ownerDocument.querySelector('table[class="ui_dialog"]').querySelector(".ui_state_highlight");dialog && dialog.click(); //ysp.appMain.reloadPage(url);
       } //点击附件方法
