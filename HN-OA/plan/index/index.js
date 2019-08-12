52a299ee512678bcdcd7e5308e6a3112164f5890
735539908 <735539908@qq.com>
2019-08-12 17:00:42 +0800

    添加其他流程

--- HN-OA/plan/index/index.js
+++ HN-OA/plan/index/index.js
@@ -18,7 +18,8 @@
       if (!elem) {
         return;
       }var data = { title: [], flag: [] };var trs = elem.querySelectorAll("tr");for (var i = 0; i < trs.length; i++) {
-        var a = [];var title = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[2].textContent;var titles = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[3].textContent;var person = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[6].textContent;var time = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[7].textContent;if (title.indexOf("发文") > -1 || title.indexOf("收文") > -1 || title.indexOf("签报") > -1) {
+        var a = [];var title = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[2].textContent;var titles = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[3].textContent;var person = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[6].textContent;var time = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[7].textContent; // if (title.indexOf("发文") > -1 || title.indexOf("收文") > -1 || title.indexOf("签报") > -1 || title.indexOf('部门会议纪要') > -1 || title.indexOf('信息变更管理')) {
+        if (title == '测试收文流程' || title == '测试发文流程' || title == '测试签报' || title == '测试部门会议纪要' || title == '测试通知公告' || title == '测试部门发文' || title == '测试人员变动' || title == '测试公司会议纪要' || title == '测试考勤审批' || title == '测试请假审批' || title == '付款测试' || title == '测试工作联系单' || title == '测试会议通知' || title == '测试车辆管理' || title == '测试信息变更') {
           a.push(titles);a.push(person);a.push(time);data.title.push(a);
         }
       }var trs = elem.querySelectorAll("tr");for (var i = 1; i < trs.length; i++) {
@@ -35,7 +36,7 @@
       if (data.eventType == "click") {
         //ysp.appMain.showLoading();
         var index = parseInt(data.customData);var a = [];var trs = elem.querySelectorAll("tr");for (var i = 0; i < trs.length; i++) {
-          var title = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[2].textContent;if (title.indexOf("发文") > -1 || title.indexOf("收文") > -1 || title.indexOf("签报") > -1) {
+          var title = trs[i].querySelectorAll("td") && trs[i].querySelectorAll("td")[2].textContent;if (title == '测试收文流程' || title == '测试发文流程' || title == '测试签报' || title == '测试部门会议纪要' || title == '测试通知公告' || title == '测试部门发文' || title == '测试人员变动' || title == '测试公司会议纪要' || title == '测试考勤审批' || title == '测试请假审批' || title == '付款测试' || title == '测试工作联系单' || title == '测试会议通知' || title == '测试车辆管理' || title == '测试信息变更') {
             a.push(trs[i]);
           }
         }a[index].querySelectorAll("td")[3].querySelector("a").click(); //elem.querySelectorAll("tr")[index].querySelectorAll("td")[1].querySelector("a").click();
