f42cad9e3ba19b8c756a8d6c321a5c80c4f43b12
735539908 <735539908@qq.com>
2019-08-22 14:47:59 +0800

    曹妃甸OA全局修改

--- HNCaoFeiDian-OA/gulpfile.js
+++ HNCaoFeiDian-OA/gulpfile.js
@@ -3,17 +3,17 @@ var gulp = require('gulp'),
     
 gulp.task('dev', async()=>{
     gulp.src(['**/*', '!./gulpfile.js', '!./lib/images/**/*', '!./**/*.png', '!./**/css/**/*', '!/**/*.ico'])
-        //OA http://oa1.chngsc.cn/ 环境转移 http://123.56.221.127:30011 环境
-        .pipe(replace(/http:\\{2}\/\\{2}\/oa1\\{2}.chngsc\\{2}.cn/gm, 'http:\\\\/\\\\/123\\\\.56\\\\.221\\\\.127:30011'))
-        .pipe(replace(/oa1\.chngsc\.cn/gm, '123.56.221.127:30011'))
+        //OA http://oa1.chngsc.cn/ 环境转移http://59.110.171.69:31003环境
+        .pipe(replace(/http:\\{2}\/\\{2}\/oa1\\{2}.chngsc\\{2}.cn/gm, 'http:\\\\/\\\\/59\\\\.110\\\\.171\\\\.69:31003'))
+        .pipe(replace(/oa1\.chngsc\.cn/gm, '59.110.171.69:31003'))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('release', async()=>{
     gulp.src(['**/*', '!./gulpfile.js', '!./lib/images/**/*', '!./**/*.png', '!./**/css/**/*', '!/**/*.ico'])
-        //OA http://123.56.221.127:30011 环境转移 http://oa1.hnrl.chng.com/ 环境
-        .pipe(replace(/http:\\{2}\/\\{2}\/123\\{2}.56\\{2}.221\\{2}.127:30011/gm, 'http:\\\\/\\\\/oa1\\\\.hnrl\\\\.chng\\\\.cn'))
-        .pipe(replace(/123\.56\.221\.127:30011/gm, 'oa1.hnrl.chng.com'))
+        //OA http://59.110.171.69:31003 环境转移 http://oa1.chngsc.cn/ 环境
+        .pipe(replace(/http:\\{2}\/\\{2}\/59\\{2}.110\\{2}.171\\{2}.69:31003/gm, 'http:\\\\/\\\\/oa1\\\\.hnrl\\\\.chng\\\\.com'))
+        .pipe(replace(/59\.110\.171\.69:31003/gm, 'oa1.hnrl.chng.com'))
         .pipe(gulp.dest('./'));
 });
 
@@ -23,4 +23,19 @@ gulp.task('20070TO30011', async()=>{
         .pipe(replace(/http:\\{2}\/\\{2}\/123\\{2}.56\\{2}.221\\{2}.127:20070/gm, 'http:\\\\/\\\\/123\\\\.56\\\\.221\\\\.127:30011'))
         .pipe(replace(/123\.56\.221\.127:20070/gm, '123.56.221.127:30011'))
         .pipe(gulp.dest('./'));
-});
+});      
+//gulp.task('dev', function() {
+    //gulp.src(['**/*', '!./gulpfile.js', '!./lib/images/**/*', '!./**/*.png', '!./**/css/**/*', '!/**/*.ico'])
+        //OA http://oa1.chngsc.cn/ 环境转移 http://123.56.221.127:20070 环境
+        //.pipe(replace(/http:\\{2}\/\\{2}\/oa1\\{2}.chngsc\\{2}.cn/gm, 'http:\\\\/\\\\/123\\\\.56\\\\.221\\\\.127:20070'))
+        //.pipe(replace(/oa1\.chngsc\.cn/gm, '123.56.221.127:20070'))
+        //.pipe(gulp.dest('./'));
+//});
+
+//gulp.task('release', function() {
+    //gulp.src(['**/*', '!./gulpfile.js', '!./lib/images/**/*', '!./**/*.png', '!./**/css/**/*', '!/**/*.ico'])
+        //OA http://123.56.221.127:20070 环境转移 http://oa1.chngsc.cn/ 环境
+        //.pipe(replace(/http:\\{2}\/\\{2}\/123\\{2}.56\\{2}.221\\{2}.127:20070/gm, 'http:\\\\/\\\\/oa1\\\\.chngsc\\\\.cn'))
+        //.pipe(replace(/123\.56\.221\.127:20070/gm, 'oa1.chngsc.cn'))
+        //.pipe(gulp.dest('./'));
+//});
