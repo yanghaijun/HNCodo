e4280eda5b5eee4d948a15679ca5f7900cc9b1cf
735539908 <735539908@qq.com>
2019-10-16 18:08:10 +0800

    修改登录跨域问题和在苹果手机上显示问题

--- HNCFD-Email/plan/index/index.js
+++ HNCFD-Email/plan/index/index.js
@@ -76,15 +76,14 @@
     getData_control9_WgSZ00: function (elem) {
       setTimeout(function () {
         var demoUrl = elem.ownerDocument.defaultView.location.href;var urlIP = demoUrl.split("afmail.nsf")[0]; // var el = elem.querySelectorAll('frame') && elem.querySelectorAll('frame')[1].contentDocument.documentElement.querySelector("#ext-gen3");
-        var name = elem.querySelector('input[name="mailfilename"]').value;var numText = elem.querySelector("#ext-comp-1010").textContent;var nums = numText.split("共")[1];var num = nums.split("条")[0];var xmlhttp = null;if (window.XMLHttpRequest) {
+        debugger;var name = elem.querySelector('input[name="mailfilename"]').value;var numText = elem.querySelector("#ext-comp-1010").textContent;var nums = numText.split("共")[1];var num = parseInt(nums.split("条")[0]);var xmlhttp = null;if (window.XMLHttpRequest) {
           xmlhttp = new XMLHttpRequest();
         } else if (window.ActiveXObject) {
           xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }xmlhttp.onreadystatechange = function () {
           if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             //alert(JSON.parse(xmlhttp.responseText).ViewData);
-            var viewData = JSON.parse(xmlhttp.responseText).ViewData; //console.log(viewData);
-            var data = [];viewData.map(function (a, b) {
+            var viewData = JSON.parse(xmlhttp.responseText).ViewData;console.log(viewData);var data = [];viewData.map(function (a, b) {
               data.push(urlIP + name + '/($Inbox)/' + a.SID + '?OpenDocument&TabID=' + a.SID + '&LinkTarget=NewTabWindow');
             });if (top.EAPI.isAndroid()) {
               top.huaneng.fujianCacheUrl(JSON.stringify({ type: 'Email', cookie: document.cookie, fujianUrl: data.toString() }));
@@ -92,7 +91,7 @@
               top.EAPI.postMessageToNative('Email', data);
             }
           }
-        };xmlhttp.open("post", urlIP + "afmail.nsf/agent_CustomViewGetData?openagent");xmlhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");xmlhttp.send("start&filterKey&viewName=($Inbox)&showUnread=true&reverse=true&dbName=" + name + '&limit=' + num);
+        };xmlhttp.open("post", urlIP + "afmail.nsf/agent_CustomViewGetData?openagent");xmlhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");xmlhttp.withCredentials = true;xmlhttp.send("start&filterKey&viewName=($Inbox)&showUnread=true&reverse=true&dbName=" + name + '&limit=' + num);
       }, 4000);
     },
     doAction_uiControl5_oOdoom: function (data, elem) {},
@@ -121,20 +120,6 @@
     getTemplate_uiControl6_TcxCRg: function () {
       var selfTemplate = 'module.exports = React.createClass({\n  componentWillMount(){\n    ysp.appMain.showLoading();\n  },\n  componentDidMount(){\n    ysp.appMain.hideLoading();\n  },\n  getValue: function(e) {\n    var target = e.target;\n    var hanlder=this.props.customHandler;\n    if(hanlder){\n      hanlder({\n        eventType:"getValue",\n        data:target.parentElement.getAttribute(\'index\')&&target.parentElement.getAttribute(\'index\')\n      })\n    }\n  },\n  render: function() {\n    var data = this.props.customData;\n    var me =this;\n    return (\n      <div className=\'d_shoujian\'>\n        <div className=\'d_shoujianHeader\'>\u6536\u4EF6\u7BB1</div>\n        <div> \n          {data.list&&data.list.map(function(i,h){\n              if(h>0){\n                 return ( <div className=\'d_indexList\' index={i.index} onClick={me.getValue}>\n                    <span style={{\'display\':i.imgs==0?\'none\':\'block\'}}></span>\n                    <span>{i.name}</span>\n                    <span>{i.date}  {i.time}</span>\n                    <span className={i.titleColor}>{i.title}</span>\n                    <span>\u5927\u5C0F\uFF1A {i.cav}</span>\n                    <span style={{\'display\':i.imgs==2?\'block\':\'none\'}}></span>\n                    <span></span>\n                </div>)\n              }    \n            })}\n          </div>\n      </div>\n    )\n  }\n});';
       return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n  componentWillMount: function componentWillMount() {\n    ysp.appMain.showLoading();\n  },\n  componentDidMount: function componentDidMount() {\n    ysp.appMain.hideLoading();\n  },\n\n  getValue: function getValue(e) {\n    var target = e.target;\n    var hanlder = this.props.customHandler;\n    if (hanlder) {\n      hanlder({\n        eventType: \"getValue\",\n        data: target.parentElement.getAttribute('index') && target.parentElement.getAttribute('index')\n      });\n    }\n  },\n  render: function render() {\n    var data = this.props.customData;\n    var me = this;\n    return React.createElement(\n      'div',\n      { className: 'd_shoujian' },\n      React.createElement(\n        'div',\n        { className: 'd_shoujianHeader' },\n        '\\u6536\\u4EF6\\u7BB1'\n      ),\n      React.createElement(\n        'div',\n        null,\n        data.list && data.list.map(function (i, h) {\n          if (h > 0) {\n            return React.createElement(\n              'div',\n              { className: 'd_indexList', index: i.index, onClick: me.getValue },\n              React.createElement('span', { style: { 'display': i.imgs == 0 ? 'none' : 'block' } }),\n              React.createElement(\n                'span',\n                null,\n                i.name\n              ),\n              React.createElement(\n                'span',\n                null,\n                i.date,\n                '  ',\n                i.time\n              ),\n              React.createElement(\n                'span',\n                { className: i.titleColor },\n                i.title\n              ),\n              React.createElement(\n                'span',\n                null,\n                '\\u5927\\u5C0F\\uFF1A ',\n                i.cav\n              ),\n              React.createElement('span', { style: { 'display': i.imgs == 2 ? 'block' : 'none' } }),\n              React.createElement('span', null)\n            );\n          }\n        })\n      )\n    );\n  }\n});";
-    },
-    getData_control11_t5SnIs: function (elem) {},
-    doAction_uiControl4_wXL5XG: function (data, elem) {
-      if (data.eventType == 'cache') {
-        if (top.EAPI.isAndroid()) {
-          top.huaneng.cleanCache('emailCleanCache');
-        } else if (top.EAPI.isIOS()) {
-          top.EAPI.postMessageToNative('emailCleanCache');
-        }
-      }
-    },
-    getTemplate_uiControl4_wXL5XG: function () {
-      var selfTemplate = "module.exports = React.createClass({\n  handlerClick(){\n    var handler = this.props.customHandler;\n    if(handler){\n      handler({\n        eventType:'cache'\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div onClick={this.handlerClick.bind(this)} style={{'marginTop':'60px'}}>\n        \u6E05\u9664\u7F13\u5B58\n      </div>\n    )\n  }\n});";
-      return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n  handlerClick: function handlerClick() {\n    var handler = this.props.customHandler;\n    if (handler) {\n      handler({\n        eventType: 'cache'\n      });\n    }\n  },\n\n  render: function render() {\n    return React.createElement(\n      'div',\n      { onClick: this.handlerClick.bind(this), style: { 'marginTop': '60px' } },\n      '\\u6E05\\u9664\\u7F13\\u5B58'\n    );\n  }\n});";
     }
   }, "index");
 })(window, ysp);
