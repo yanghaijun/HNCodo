023185043f5b88dfff2c299c90471250d615f7a7
735539908 <735539908@qq.com>
2019-12-25 18:01:55 +0800

    ERP提交

--- ERP1/plan/index/index.js
+++ ERP1/plan/index/index.js
@@ -3,15 +3,45 @@
     getData_control4_PAmTZG: function (elem) {
       if (!elem) {
         return;
-      }var divs = elem.ownerDocument.querySelector("#DetailedNavigationTree") && elem.ownerDocument.querySelector("#DetailedNavigationTree").querySelectorAll('div');for (var i = 0; i < divs.length; i++) {
-        if (divs[i].textContent == '待办工作') {
-          var evt = document.createEvent('MouseEvent');evt.initMouseEvent('click', true, true);divs[i].querySelector('a').dispatchEvent(evt); //divs[i].querySelector('a').click();
-        }
+      }ysp.appMain.showLoading(); // debugger;
+      // var divs = elem.ownerDocument.querySelector("#DetailedNavigationTree") && elem.ownerDocument.querySelector("#DetailedNavigationTree").querySelectorAll('div');
+      // for (var i = 0; i < divs.length; i++) {
+      //   if (divs[i].textContent == '新待办工作') {
+      //     var evt = document.createEvent('MouseEvent');
+      //     evt.initMouseEvent('click', true, true);
+      //     divs[i].querySelector('a').dispatchEvent(evt); //divs[i].querySelector('a').click();
+      //   }
+      // }
+    }, doAction_uiControl3_vTJhaf: function (data, elem) {
+      if (data.eventType == 'click') {
+        debugger;setTimeout(function () {
+          var divs = elem.ownerDocument.querySelector("#DetailedNavigationTree") && elem.ownerDocument.querySelector("#DetailedNavigationTree").querySelectorAll('div');for (var i = 0; i < divs.length; i++) {
+            if (divs[i].textContent == '新待办工作') {
+              var evt = document.createEvent('MouseEvent');evt.initMouseEvent('click', true, true);divs[i].querySelector('a').dispatchEvent(evt); //divs[i].querySelector('a').click();
+            }
+          }
+        }, 2000);
       }
-    }, doAction_uiControl3_vTJhaf: function (data, elem) {},
+    },
     getTemplate_uiControl3_vTJhaf: function () {
-      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    return (\n      <div>\n        \u81EA\u5B9A\u4E49\u7EC4\u4EF6\u7528\u6765\u9002\u914D\u57FA\u672C\u7EC4\u4EF6\u65E0\u6CD5\u9002\u914D\u7684\u9875\u9762\u5143\u7D20\uFF0C\u60A8\u53EF\u4EE5\u901A\u8FC7\u53F3\u952E\u6253\u5F00\u8BE5\u81EA\u5B9A\u4E49\u7EC4\u4EF6\u7F16\u8F91\u5668\u8FDB\u884C\u7F16\u8F91\n      </div>\n    )\n  }\n});";
-      return "\"use strict\";\n\nmodule.exports = React.createClass({\n  displayName: \"exports\",\n\n  render: function render() {\n    return React.createElement(\n      \"div\",\n      null,\n      \"\\u81EA\\u5B9A\\u4E49\\u7EC4\\u4EF6\\u7528\\u6765\\u9002\\u914D\\u57FA\\u672C\\u7EC4\\u4EF6\\u65E0\\u6CD5\\u9002\\u914D\\u7684\\u9875\\u9762\\u5143\\u7D20\\uFF0C\\u60A8\\u53EF\\u4EE5\\u901A\\u8FC7\\u53F3\\u952E\\u6253\\u5F00\\u8BE5\\u81EA\\u5B9A\\u4E49\\u7EC4\\u4EF6\\u7F16\\u8F91\\u5668\\u8FDB\\u884C\\u7F16\\u8F91\"\n    );\n  }\n});";
+      var selfTemplate = "module.exports = React.createClass({\n  // componentDidMount(){\n  //   var handler = this.props.customHandler;\n  //   if(handler){\n  //     handler({\n  //       eventType:'click'\n  //     })\n  //   }\n  // },\n  componentDidUpdate(){\n    var handler = this.props.customHandler;\n    if(handler){\n      handler({\n        eventType:'click'\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div>\n      </div>\n    )\n  }\n});";
+      return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n\n  // componentDidMount(){\n  //   var handler = this.props.customHandler;\n  //   if(handler){\n  //     handler({\n  //       eventType:'click'\n  //     })\n  //   }\n  // },\n  componentDidUpdate: function componentDidUpdate() {\n    var handler = this.props.customHandler;\n    if (handler) {\n      handler({\n        eventType: 'click'\n      });\n    }\n  },\n\n  render: function render() {\n    return React.createElement('div', null);\n  }\n});";
+    },
+    getData_control5_T8HU3v: function (elem) {
+      if (!elem) {
+        return;
+      }setTimeout(function () {
+        ysp.appMain.hideLoading();
+      }, 2000);var data = [];var td = elem.ownerDocument.querySelector('td[id="aaaa.MyUWLCompView.Table_0-content"]');var tbody = td && td.querySelector('table') && td.querySelector('table').querySelector('tbody');var trs = tbody && tbody.querySelectorAll('tr');for (var i = 0; i < trs.length; i++) {
+        var obj = {};if (trs[i].getAttribute("userdata")) {
+          obj.liuchenghao = trs[i].querySelectorAll('td')[1].textContent;obj.youxianji = trs[i].querySelectorAll('td')[2].textContent;obj.yewumiaoshu = trs[i].querySelectorAll('td')[3].textContent;obj.yewuliucheng = trs[i].querySelectorAll('td')[4].textContent;obj.shengqingren = trs[i].querySelectorAll('td')[5].textContent;obj.fasongriqi = trs[i].querySelectorAll('td')[6].textContent;obj.gongsimingcheng = trs[i].querySelectorAll('td')[7].textContent;obj.buzhoumiaoshu = trs[i].querySelectorAll('td')[8].textContent;obj.liuchenglaiyuan = trs[i].querySelectorAll('td')[9].textContent;obj.gongsibianma = trs[i].querySelectorAll('td')[10].textContent;data.push(obj);
+        }
+      }return data;
+    },
+    doAction_uiControl4_AnNOzG: function (data, elem) {},
+    getTemplate_uiControl4_AnNOzG: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    var data = this.props.customData || [];\n    return (\n      <div>\n        {\n          data && data.map(function(d,i){\n            return(\n            \t<div className=\"erp_index\">\n                <div>{d.liuchenghao}</div>\n                <div>{d.youxianji}</div>\n                <div>{d.yewumiaoshu}</div>\n                <div>{d.yewuliucheng}</div>\n                <div>{d.shengqingren}</div>\n                <div>{d.fasongriqi}</div>\n                <div>{d.gongsimingcheng}</div>\n                <div>{d.buzhoumiaoshu}</div>\n                <div>{d.liuchenglaiyuan}</div>\n                <div>{d.gongsibianma}</div>\n              </div>\n            )\n          })\n        }\n      </div>\n    )\n  }\n});";
+      return "\"use strict\";\n\nmodule.exports = React.createClass({\n  displayName: \"exports\",\n\n  render: function render() {\n    var data = this.props.customData || [];\n    return React.createElement(\n      \"div\",\n      null,\n      data && data.map(function (d, i) {\n        return React.createElement(\n          \"div\",\n          { className: \"erp_index\" },\n          React.createElement(\n            \"div\",\n            null,\n            d.liuchenghao\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.youxianji\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.yewumiaoshu\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.yewuliucheng\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.shengqingren\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.fasongriqi\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.gongsimingcheng\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.buzhoumiaoshu\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.liuchenglaiyuan\n          ),\n          React.createElement(\n            \"div\",\n            null,\n            d.gongsibianma\n          )\n        );\n      })\n    );\n  }\n});";
     }
   }, "index");
 })(window, ysp);
