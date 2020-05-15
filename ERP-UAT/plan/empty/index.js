7d08b7180b860bfac0e7645b26162c2d7d73083f
735539908 <735539908@qq.com>
2020-05-15 09:34:52 +0800

    提交

---
+++ ERP-UAT/plan/empty/index.js
@@ -0,0 +1,12 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control12_8202fp: function (elem) {
+      elem.ownerDocument.defaultView.close();
+    },
+    doAction_uiControl8_ZVh5nG: function (data, elem) {},
+    getTemplate_uiControl8_ZVh5nG: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    return (\n      <div>\n        \n      </div>\n    )\n  }\n});";
+      return "\"use strict\";\n\nmodule.exports = React.createClass({\n  displayName: \"exports\",\n\n  render: function render() {\n    return React.createElement(\"div\", null);\n  }\n});";
+    }
+  }, "empty");
+})(window, ysp);
