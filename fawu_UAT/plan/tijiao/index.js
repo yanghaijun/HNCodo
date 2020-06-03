2b55996e2a0ac63591ccc744d51be98ffef09337
735539908 <735539908@qq.com>
2020-06-03 17:26:50 +0800

    提交

---
+++ fawu_UAT/plan/tijiao/index.js
@@ -0,0 +1,27 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control16_ZGzmPn: function (elem) {
+      if (!elem) {
+        return;
+      } //console.log(elem);
+      var data = { person: [], post: [] };var trs = elem.querySelectorAll("tr");for (var i = 0; i < trs.length; i++) {
+        var a = [];var b = [];if (trs[i].getAttribute('udat')) {
+          var tds = trs[i].querySelectorAll('td');a.push(tds[tds.length - 2].textContent);b.push(tds[tds.length - 1].textContent);data.person.push(a);data.post.push(b);
+        }
+      }return data;
+    },
+    doAction_uiControl14_GuT1ou: function (data, elem) {
+      var type = data.eventType;var index = parseInt(data.customData);
+      if (type == 'click') {
+        var trs = elem.querySelectorAll("tr");var a = [];for (var i = 0; i < trs.length; i++) {
+          if (trs[i].getAttribute('udat')) {
+            a.push(trs[i]);
+          }
+        }a[index] && a[index].querySelector('input').click();
+      }
+    }, getTemplate_uiControl14_GuT1ou: function () {
+      var selfTemplate = 'module.exports = React.createClass({\n  handlerClick(e){\n    var handler = this.props.customHandler;\n    var target = e.target;\n    if(handler){\n      handler({\n        eventType:\'click\',\n        data:target.dataset.index\n      })\n    }\n  },\n  render: function() {\n    var _this = this;\n    var dataPerson = this.props.customData && this.props.customData.person;\n    var dataPost = this.props.customData && this.props.customData.post;\n    return (\n      <div>\n        {\n          dataPerson.map(function(d,i){\n            return(\n            \t<div onClick={_this.handlerClick.bind(_this)} data-index={i}>\n                <span data-index={i}>{d}</span>\n                <span data-index={i}>{dataPost[i]}</span>\n              </div>\n            )\n          })\n        }\n      </div>\n    )\n  }\n});';
+      return '\'use strict\';\n\nmodule.exports = React.createClass({\n  displayName: \'exports\',\n  handlerClick: function handlerClick(e) {\n    var handler = this.props.customHandler;\n    var target = e.target;\n    if (handler) {\n      handler({\n        eventType: \'click\',\n        data: target.dataset.index\n      });\n    }\n  },\n\n  render: function render() {\n    var _this = this;\n    var dataPerson = this.props.customData && this.props.customData.person;\n    var dataPost = this.props.customData && this.props.customData.post;\n    return React.createElement(\n      \'div\',\n      null,\n      dataPerson.map(function (d, i) {\n        return React.createElement(\n          \'div\',\n          { onClick: _this.handlerClick.bind(_this), \'data-index\': i },\n          React.createElement(\n            \'span\',\n            { \'data-index\': i },\n            d\n          ),\n          React.createElement(\n            \'span\',\n            { \'data-index\': i },\n            dataPost[i]\n          )\n        );\n      })\n    );\n  }\n});';
+    }
+  }, "tijiao");
+})(window, ysp);
---
+++ fawu_UAT/plan/zuofei/index.js
@@ -0,0 +1,22 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control14_rX2tuW: function (elem) {
+      if (!elem) {
+        return;
+      }
+    },
+    doAction_uiControl12_kWtpO7: function (data, elem) {
+      var type = data.eventType;var title = data.customData;var ass = elem.querySelectorAll('a');if (ass) {
+        if (title == '确定') {
+          ass[ass.length - 2].click();
+        } else {
+          ass[ass.length - 1].click();
+        }
+      }
+    },
+    getTemplate_uiControl12_kWtpO7: function () {
+      var selfTemplate = 'module.exports = React.createClass({\n  handlerClick(e){\n    var handler = this.props.customHandler;\n    var target = e.target;\n    if(handler){\n      handler({\n        eventType:\'click\',\n        data:target.dataset.title\n      })\n    }\n  },\n  render: function() {\n    return (\n      <div>\n        <div>\u786E\u8BA4\u4F5C\u5E9F\u5F53\u524D\u5408\u540C\uFF1F</div>\n        \n        <div>\n          <span onClick={this.handlerClick.bind(this)} data-title = "\u786E\u5B9A">\u786E\u5B9A</span>\n          <span onClick={this.handlerClick.bind(this)} data-title = "\u53D6\u6D88">\u53D6\u6D88</span>\n        </div>\n      </div>\n    )\n  }\n});';
+      return "\"use strict\";\n\nmodule.exports = React.createClass({\n  displayName: \"exports\",\n  handlerClick: function handlerClick(e) {\n    var handler = this.props.customHandler;\n    var target = e.target;\n    if (handler) {\n      handler({\n        eventType: 'click',\n        data: target.dataset.title\n      });\n    }\n  },\n\n  render: function render() {\n    return React.createElement(\n      \"div\",\n      null,\n      React.createElement(\n        \"div\",\n        null,\n        \"\\u786E\\u8BA4\\u4F5C\\u5E9F\\u5F53\\u524D\\u5408\\u540C\\uFF1F\"\n      ),\n      React.createElement(\n        \"div\",\n        null,\n        React.createElement(\n          \"span\",\n          { onClick: this.handlerClick.bind(this), \"data-title\": \"\\u786E\\u5B9A\" },\n          \"\\u786E\\u5B9A\"\n        ),\n        React.createElement(\n          \"span\",\n          { onClick: this.handlerClick.bind(this), \"data-title\": \"\\u53D6\\u6D88\" },\n          \"\\u53D6\\u6D88\"\n        )\n      )\n    );\n  }\n});";
+    }
+  }, "zuofei");
+})(window, ysp);
