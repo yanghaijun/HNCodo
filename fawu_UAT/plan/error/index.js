bf1a814b45e66fe7e99d6c423fb090f9342c15fc
735539908 <735539908@qq.com>
2020-06-30 15:20:47 +0800

    0630 提交

---
+++ fawu_UAT/plan/error/index.js
@@ -0,0 +1,14 @@
+(function (win, ysp) {
+  ysp.runtime.Model.extendLoadingModel({
+    getData_control34_qTJ0T3: function (elem) {
+      if (!elem) {
+        return;
+      }
+    },
+    doAction_uiControl32_PgWySm: function (data, elem) {},
+    getTemplate_uiControl32_PgWySm: function () {
+      var selfTemplate = "module.exports = React.createClass({\n  render: function() {\n    return (\n      <div style={{'text-align':'center','padding':'10px','color':'red'}}>\n        \u7531\u4E8E\u539F\u9875\u9762\u51FA\u73B0\u9519\u8BEF\uFF0C\u6545\u8BF7\u60A8\u9000\u51FA\u7CFB\u7EDF\u91CD\u65B0\u767B\u5F55\uFF01\uFF01\n      </div>\n    )\n  }\n});";
+      return "'use strict';\n\nmodule.exports = React.createClass({\n  displayName: 'exports',\n\n  render: function render() {\n    return React.createElement(\n      'div',\n      { style: { 'text-align': 'center', 'padding': '10px', 'color': 'red' } },\n      '\\u7531\\u4E8E\\u539F\\u9875\\u9762\\u51FA\\u73B0\\u9519\\u8BEF\\uFF0C\\u6545\\u8BF7\\u60A8\\u9000\\u51FA\\u7CFB\\u7EDF\\u91CD\\u65B0\\u767B\\u5F55\\uFF01\\uFF01'\n    );\n  }\n});";
+    }
+  });
+})(window, ysp);
